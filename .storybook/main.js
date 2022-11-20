d5974f5179a1bebab83f8d89eb90c34c0e60d15b
Illiasse RIFKI <i.rifki@oneprog.fr>
2022-11-20 02:16:08 +0100

    feat: add button component / storybook for web working

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,5 +7,6 @@ module.exports = {
       '@storybook/addon-ondevice-controls',
       '@storybook/addon-ondevice-backgrounds',
       '@storybook/addon-ondevice-actions',
+      '@storybook/addon-react-native-web',
     ],
   };
--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -1,32 +1,176 @@
-import { createUseStyles } from '@hooks';
-import { useTheme } from '@react-navigation/native';
-import { ThemeTypes } from '@themes';
-import { View, StyleProp, ViewStyle } from 'react-native';
+import React from 'react';
+import {
+  View,
+  StyleProp,
+  ViewStyle,
+  TouchableOpacity,
+  Text,
+  TextStyle,
+} from 'react-native';
+import {
+  createUseStyles,
+  useTheme,
+  ThemeType,
+  BorderRadiusTypes,
+  ButtonSizeTypes,
+} from '@styles';
 
-interface DataProps {}
+interface iconOptionsType {
+  name?: string;
+  size?: number;
+  color?: string;
+}
+
+interface titleOptionsType {
+  size?: keyof ButtonSizeTypes;
+  position?: 'center' | 'left' | 'right';
+  color?: string;
+  weight?: 'normal' | 'bold';
+  flex?: number,
+}
+
+interface DataProps {
+  children?: React.ReactNode;
+  onPress?: () => void;
+  smallWithIcon?: boolean;
+}
 
 interface ContextProps {
-  theme?: ThemeTypes;
-  hovered?: boolean;
+  theme?: ThemeType;
+  disabled?: boolean;
+  size?: 's' | 'm' | 'l';
+  backgroundColor?: string;
+  title?: string;
+  titleOptions?: titleOptionsType;
+  outlined?: boolean;
+  rounded?: keyof BorderRadiusTypes;
+  leftIcon?: iconOptionsType;
+  rightIcon?: iconOptionsType;
+  loading?: boolean;
+  elevation?: number;
 }
 
 interface StyleTypes {
   containerStyle: StyleProp<ViewStyle>;
+  leftIconStyle: StyleProp<ViewStyle>;
+  rightIconStyle: StyleProp<ViewStyle>;
+  contentStyle: StyleProp<ViewStyle>;
+  titleStyle: StyleProp<TextStyle>;
 }
 
-export const Button: React.FC<DataProps & ContextProps> = (props) => {
-  const styles = useStyles({ theme: useTheme(), hovered: props.hovered });
+export const Button: React.FC< DataProps & ContextProps> = ({
+  onPress,
+  children,
+  disabled = false,
+  size,
+  title = 'Button',
+  titleOptions,
+  backgroundColor,
+  rounded,
+  leftIcon,
+  rightIcon,
+  loading,
+  outlined,
+  elevation,
+  smallWithIcon,
+  ...props
+}) => {
+  const theme = useTheme();
+  const styles = useStyles({
+    theme,
+    disabled,
+    size,
+    title,
+    titleOptions,
+    backgroundColor,
+    rounded,
+    rightIcon,
+    leftIcon,
+    loading,
+    outlined,
+    elevation,
+  });
 
-  return <View style={styles.containerStyle} />;
+  return (
+    <TouchableOpacity
+      disabled={disabled}
+      onPress={onPress}
+      style={styles.containerStyle}
+      {...props}>
+      {loading ? (
+        <></>
+      ) : children ? (
+        children
+      ) : (
+        <>
+          {leftIcon?.name ? (
+            <View style={styles.leftIconStyle}>
+            </View>
+          ) : rightIcon && !smallWithIcon ? (
+            <View style={styles.leftIconStyle} />
+          ) : null}
+
+          <View style={styles.contentStyle}>
+            <Text style={styles.titleStyle}>{title}</Text>
+          </View>
+
+          {rightIcon?.name ? (
+            <View style={styles.rightIconStyle}>
+            </View>
+          ) : leftIcon && !smallWithIcon ? (
+            <View style={styles.rightIconStyle} />
+          ) : null}
+        </>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+const getHeight = (size: 's' | 'm' | 'l' | undefined) => {
+  switch (size) {
+  case 's':
+    return 20;
+  case 'm':
+    return 40;
+  case 'l':
+    return 50;
+  default:
+    return 50;
+  }
 };
 
 const getStyles = (context: ContextProps): StyleTypes => ({
   containerStyle: {
-    height: 300,
-    width: '100%',
-    backgroundColor: context.hovered
-      ? context.theme?.colors.border
-      : context.theme?.colors.notification,
+    opacity: context.disabled ?  0.6 : 1,
+    backgroundColor:
+    context.backgroundColor || (context.outlined ? 'transparent' :
+      context.theme?.colors.basics.black),
+    minHeight: getHeight(context.size),
+    flexDirection: 'row',
+    borderRadius: context.theme?.metrics.borderRadius[context.rounded || 's'],
+    padding: context.size === 's' ? 5 : 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: context.elevation || 0 },
+    shadowOpacity: context.elevation ? 0.5 : 0,
+    shadowRadius: 2,
+    elevation: context.elevation || 0,
+    alignContent: 'center',
+    justifyContent: 'center',
+  },
+  leftIconStyle: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentStyle: { flex: context.titleOptions?.flex || 3, justifyContent: 'center' },
+  rightIconStyle: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  titleStyle: {
+    color: context.titleOptions?.color || (context.outlined ?  context.theme?.colors.basics.black :
+      context.theme?.colors.basics.white),
+    fontSize:
+      context.theme?.fonts.fontSize.button[context.titleOptions?.size || 't1'],
+    textAlign: context.titleOptions?.position || 'center',
+    fontWeight: context.titleOptions?.weight || 'bold',
   },
 });
 
