da966a5ad0417d14866f3dc894ae2f09e967066f
Illiasse RIFKI <i.rifki@oneprog.fr>
2022-11-20 15:21:41 +0100

    feat: font oblivian and splash

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
-import { AppNavigator } from '@navigation';
-import {AppRegistry} from 'react-native';
-import * as React from 'react';
+import { AppNavigator } from "@navigation";
+import * as React from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { View } from "react-native";
+
+SplashScreen.preventAutoHideAsync();
 
 function App() {
-  return <AppNavigator />;
+  const [fontsLoaded] = useFonts({
+    "Oblivian-Light": require("@assets/fonts/Oblivian/Oblivian-Light.otf"),
+    "Oblivian-Regular": require("@assets/fonts/Oblivian/Oblivian-Medium.otf"),
+    "Oblivian-Bold": require("@assets/fonts/Oblivian/Oblivian-Bold.otf"),
+  });
+
+  const onLayoutRootView = React.useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  return (
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <AppNavigator />
+    </View>
+  );
 }
 
 export default App;
